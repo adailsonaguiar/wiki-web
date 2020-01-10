58fd6371f7917894b188925e525f05914434469f
Adailson Aguiar <adailsonacj@live.com>
2020-01-10 10:27:54 -0300

    salvando atividades no firebase

--- src/pages/home-page/Home.js
+++ src/pages/home-page/Home.js
@@ -1,30 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../../components/header-component/Header';
 import firebase from 'firebase';
-import { Container } from './styles';
+import { Container, Form, Row, TitleForm } from './styles';
+import Load from '../../components/load-component/Load';
 
 const Home = () => {
   const [userId, setUserId] = useState('');
+  const [titlePost, setTitlePost] = useState('');
+  const [typePost, setTypePost] = useState('');
+  const [descriptionPost, setDescriptionPost] = useState('');
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     if (localStorage.getItem('user')) {
       setUserId(JSON.parse(localStorage.getItem('user')).user.uid);
     }
-  });
+  }, []);
 
-  const saveActivity = () => {
-    firebase
-      .database()
-      .ref('activities/' + userId)
-      .set({
-        teste: 5,
-        oto: 3
-      });
+  const cleanForm = () => {
+    setTitlePost('');
+    setDescriptionPost('');
+    setTypePost('');
   };
+
+  const validateForm = () => {
+    if (
+      titlePost.length > 0 &&
+      descriptionPost.length > 0 &&
+      typePost.length > 0
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const saveActivity = async () => {
+    if (validateForm()) {
+      setLoading(true);
+      const timestamp = new Date().getTime();
+      const body = { titlePost, typePost, descriptionPost };
+      await firebase
+        .database()
+        .ref(`activities/${userId}/${timestamp}`)
+        .set(body);
+      setLoading(false);
+      cleanForm();
+    } else {
+      alert('Preencha os campos corretamente!');
+    }
+  };
+
   return (
     <Container>
       <Header />
-      <h1>Home Works</h1>
-      <button onClick={() => saveActivity()}>teste</button>
+      <Form>
+        <Row>
+          <TitleForm>Novo Post</TitleForm>
+        </Row>
+        <Row>
+          <select value={typePost} onChange={e => setTypePost(e.target.value)}>
+            <option>Selecione o tipo de atividade</option>
+            <option>Páginas</option>
+            <option>Documentos</option>
+            <option>Tutoriais</option>
+            <option>Reuniões</option>
+          </select>
+          <input
+            placeholder='Digite o título de sua atividade'
+            value={titlePost}
+            onChange={e => setTitlePost(e.target.value)}
+          />
+        </Row>
+        <Row>
+          <textarea
+            placeholder='Digite o conteúdo da sua atividade'
+            value={descriptionPost}
+            onChange={e => setDescriptionPost(e.target.value)}
+          />
+        </Row>
+        <Row className='center'>
+          {loading ? (
+            <Load />
+          ) : (
+            <button onClick={() => saveActivity()}>SALVAR</button>
+          )}
+        </Row>
+      </Form>
+      ><h1>Home Works</h1>
     </Container>
   );
 };
--- src/pages/home-page/styles.js
+++ src/pages/home-page/styles.js
@@ -4,15 +4,87 @@ export const Container = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   background: linear-gradient(rgba(83, 107, 130, 0.4), rgba(49, 60, 71, 0.9)),
     url('https://cdn.onthewall.com.br/media/catalog/product/cache/1/image/17f82f742ffe127f42dca9de82fb58b1/l/u/luzes_da_cidade.jpg')
       no-repeat fixed;
   background-size: cover;
+  padding-top: 60px;
 
   h2 {
     color: white;
     margin-bottom: 5px;
   }
+`;
+
+export const Form = styled.div`
+  background: #fff;
+  width: 70%;
+  border-radius: 5px;
+  box-shadow: 6px 7px 15px rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+
+  select {
+    background: #fff;
+    border-radius: 4px;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+
+  input {
+    border: none;
+    padding-left: 10px;
+    outline: none;
+    width: 280px;
+    height: 40px;
+    font-size: 16px;
+    color: #000;
+    background: transparent;
+    border-bottom-width: 1px;
+    border-bottom: solid #c48f12 1px;
+  }
+
+  textarea {
+    width: 100%;
+    height: 60px;
+    padding: 10px;
+    border: solid #c48f12 1px;
+    border-radius: 4px;
+  }
+
+  button {
+    margin-top: 20px;
+    border-radius: 24px;
+    border: none;
+    width: 270px;
+    height: 45px;
+    outline: none;
+    background: #c48f12;
+    color: #fff;
+    font-size: 13px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    background: #c9961e;
+    box-shadow: 3px 5px 15px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+  }
+
+  .center {
+    justify-content: center;
+  }
+`;
+
+export const TitleForm = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
 `;
